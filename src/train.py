b7fef784391c08444ddd01578f00da368b2b55c7
oke <okayenu@gmail.com>
2025-09-29 11:42:37 +0000

    Update mt( implementation.

--- src/train.py
+++ src/train.py
@@ -230,3 +230,5 @@ def log_training_report(history, report_path: str = None):
 # [2025-09-23 1:44 PM] Task 21: Add linear warmup for first 5% of total steps
 
 # [2025-09-23 3:22 PM] Task 21: Integrate LearningRateScheduler callback in get_callbacks()
+
+# [2025-09-29 9:14 AM] Task 29: Call tf.keras.mixed_precision.set_global_policy('mixed_float16')
